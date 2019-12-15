d2a39011b7646dbebf9e5f312a1d321e751e87a3
Hrishikesh <handsomehrishi@gmail.com>
2019-12-15 19:59:14 +0530

    [GCI-riskycase] Add /info endpoint

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const express = require('express');
 // create new express app and save it as "app"
 const app = express();
 
+//import package.json
+const pack = require('./package');
+
 // server configuration
 const PORT = 8080;
 
 // create a route for the app
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+//endpoint info to return a json
+app.get('/info', (req, res) => {
+  res.json({ serverName:pack.name, serverVersion:pack.version });
 });
 
 // make the server listen to requests
